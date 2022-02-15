b458af45be80f9d2444269fb737f9997dfac8f18
Olar Alex <olaralex666@gmail.com>
2022-02-15 16:22:54 +0100

    added is_inverted, normalization, inversion

--- src/data.py
+++ src/data.py
@@ -43,9 +43,30 @@ class ImageProcessor:
             "score_model": networks[2]
         }
 
+    def is_inverted(self, image):
+        height, width = image.shape
+        corner_size = (int(height*self.config.corner_ratio), int(width*self.config.corner_ratio))
+        tl = image[:corner_size[0], :corner_size[1]].flatten()
+        bl = image[-corner_size[0]:, :corner_size[1]].flatten()
+        tr = image[:corner_size[0], -corner_size[1]:].flatten()
+        br = image[-corner_size[0]:, -corner_size[1]:].flatten()
+        if self.config.use_bottom:
+            corners = np.hstack([tl, bl, tr, br])
+        else:
+            corners = np.hstack([tl, tr])
+                
+        center = image[
+            int(height*(0.5-self.config.center_ratio)):int(height*(0.5+self.config.center_ratio)),
+            int(width*(0.5-self.config.center_ratio)):int(width*(0.5+self.config.center_ratio))
+        ]
+        return center.mean() < corners.mean()
+
     def load_image(self, path):
         img = cv2.imread(path, 0)
-        img = cv2.equalizeHist(img)
+        return img
+
+    def normlize_image(self, image):
+        img = cv2.equalizeHist(image)
         img = cv2.resize(img, (self.config.img_size, self.config.img_size),
                         interpolation=cv2.INTER_AREA)
         return img
@@ -53,12 +74,19 @@ class ImageProcessor:
     def process_image(self, path):
         name = Path(path).stem
         image = self.load_image(path)
+        inverted = False
+        if self.is_inverted(image):
+            inverted = True
+            image = np.max(image) - image
+        image = self.normlize_image(image)
         image = np.array(image) / 255.
         aligned_image = self.nets["alignment_model"].predict(
             np.expand_dims(image, [0, -1]))
-        cv2.imwrite(os.path.join(self.config.output_base_path, f'{name}_aligned_img.png'), aligned_image[0] * 255.)
+        Path(os.path.join(self.config.output_base_path, 'aligned')).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(self.config.output_base_path, 'segmented')).mkdir(parents=True, exist_ok=True)
+        cv2.imwrite(os.path.join(self.config.output_base_path, 'aligned', f'{name}_aligned_img.png' if not inverted else f'inverted_{name}_aligned_img.png'), aligned_image[0] * 255.)
         segmented_image = self.nets["segmentation_model"].predict(aligned_image)
-        cv2.imwrite(os.path.join(self.config.output_base_path, f'{name}_segm_img.png'), segmented_image[0] * 255.)
+        cv2.imwrite(os.path.join(self.config.output_base_path, 'segmented', f'{name}_segm_img.png' if not inverted else f'inverted_{name}_segm_img.png'), segmented_image[0] * 255.)
         return {
             "aligned_image" : aligned_image[0],
             "segmented_image": segmented_image[0]

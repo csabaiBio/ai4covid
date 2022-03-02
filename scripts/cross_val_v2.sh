a42855329ea56227fcbafa0d523300b1772c570b
Olar Alex <olaralex666@gmail.com>
2022-03-02 20:32:57 +0100

    .

--- scripts/cross_val_v2.sh
+++ scripts/cross_val_v2.sh
@@ -1,8 +1,7 @@
 cuda=$1
 
-
-CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=0 ++img_size=512 ++steps_per_epoch=500 ++batch_size=16 &
-CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=1 ++img_size=512 ++steps_per_epoch=500 ++batch_size=16
-CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=2 ++img_size=512 ++steps_per_epoch=500 ++batch_size=16 &
-CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=3 ++img_size=512 ++steps_per_epoch=500 ++batch_size=16
-CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=4 ++img_size=512 ++steps_per_epoch=500 ++batch_size=16
+CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=0 &
+CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=1 &
+CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=2 &
+CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=3 &
+CUDA_VISIBLE_DEVICES=$cuda python run_hyperopt.py --config-name=train_v2.yaml ++fold=4

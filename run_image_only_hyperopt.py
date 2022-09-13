c0800b0f1be6e4751cceae3fe457e49c3ec0db41
Olar Alex <olaralex666@gmail.com>
2022-09-13 11:25:42 +0200

    rerunning expariments for the paper

--- run_image_only_hyperopt.py
+++ run_image_only_hyperopt.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from pathlib import Path
 
 import hydra
+import numpy as np
 import omegaconf
 import tensorflow as tf
 from omegaconf import DictConfig
@@ -25,54 +26,54 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 def run_experiments(config: DictConfig):
     wandb.init(project=config.project, entity="elte-ai4covid")
 
-    for fold in range(5):
-        datasets = generate_data(config, fold=fold)
+    fold = np.random.randint(0, 5) if config.fold == -1 else config.fold
+    datasets = generate_data(config, fold=fold)
 
-        model = build_model(config)
+    model = build_model(config)
 
-        chkpt_dir = (
-            Path(config.raw_output_base)
-            / "checkpoints_image_only"
-            / datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f")
-        )
+    chkpt_dir = (
+        Path(config.raw_output_base)
+        / "checkpoints_image_only"
+        / datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f")
+    )
 
-        Path(chkpt_dir).mkdir(exist_ok=True, parents=True)
-        chkpt_path = Path(chkpt_dir) / "cp.ckpt"
+    Path(chkpt_dir).mkdir(exist_ok=True, parents=True)
+    chkpt_path = Path(chkpt_dir) / "cp.ckpt"
 
-        omegaconf.OmegaConf.save(config=config, f=Path(chkpt_dir) / "config.yaml")
+    omegaconf.OmegaConf.save(config=config, f=Path(chkpt_dir) / "config.yaml")
 
-        with open(os.path.join(chkpt_dir, "fold"), "w") as fp:
-            fp.write(str(fold))
+    with open(os.path.join(chkpt_dir, "fold"), "w") as fp:
+        fp.write(str(fold))
 
-        model.summary()
+    model.summary()
 
-        cp_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath=chkpt_path,
-            save_weights_only=True,
-            verbose=0,
-            monitor="val_balanced_accuracy",
-            save_freq="epoch",
-            mode="max",
-        )
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=chkpt_path,
+        save_weights_only=True,
+        verbose=0,
+        monitor="val_balanced_accuracy",
+        save_freq="epoch",
+        mode="max",
+    )
 
-        early_stopping = tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss",
-            min_delta=0,
-            patience=5,
-            verbose=0,
-            mode="min",
-            baseline=None,
-            restore_best_weights=True,
-        )
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss",
+        min_delta=0,
+        patience=5,
+        verbose=0,
+        mode="min",
+        baseline=None,
+        restore_best_weights=True,
+    )
 
-        _ = model.fit(
-            datasets["train_dataset"],
-            validation_data=datasets["validation_dataset"],
-            epochs=config.epochs,
-            steps_per_epoch=config.steps_per_epoch,
-            callbacks=[WandbCallback(), cp_callback, early_stopping],
-            verbose=1,
-        )
+    _ = model.fit(
+        datasets["train_dataset"],
+        validation_data=datasets["validation_dataset"],
+        epochs=config.epochs,
+        steps_per_epoch=config.steps_per_epoch,
+        callbacks=[WandbCallback(), cp_callback, early_stopping],
+        verbose=1,
+    )
 
 
 if __name__ == "__main__":

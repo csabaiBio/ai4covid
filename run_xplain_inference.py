b5dbb9fa32607d19dd7a385fb9105784f9af6e97
Olar Alex <olaralex666@gmail.com>
2022-10-14 17:56:09 +0200

    .

--- run_xplain_inference.py
+++ run_xplain_inference.py
@@ -25,6 +25,7 @@ def plot_attention(
     chkpt_dir,
     threshold=False,
     only_correlating_cols=False,
+    global_threshold=False,
 ):
     if type(image) == np.ndarray:
         temp_image = image
@@ -33,7 +34,7 @@ def plot_attention(
 
     n_features = len(config.datasets[config.dataset_identifier].feature_cols)
 
-    fig = plt.figure(figsize=(20, 20))
+    fig = plt.figure(figsize=(40, 40))
 
     if only_correlating_cols:
         col_indicies = [5, 8, 10, 11, 14, 16, 18, 19, 20, 21, 22, 25, 26, 27, 29, 31]
@@ -59,37 +60,112 @@ def plot_attention(
         n_features = len(features_to_keep)
         j = 0
 
+    global_minimum = np.min(attention_plot)
+    global_maximum = np.max(attention_plot)
+
+    if only_correlating_cols:
+        attentions = []
+        for feat in features_to_keep:
+            if feat[0] not in col_indicies:
+                attentions.append(attention_plot[:, feat[0]])
+        attentions = np.array(attentions)
+        global_minimum = np.min(attentions)
+        global_maximum = np.max(attentions)
+
+    print(f"Global minimum : {global_minimum}, global maximum : {global_maximum}")
+
     for i in range(n_features):
         if only_correlating_cols:
             if not (features_to_keep[i][0] in col_indicies):
                 temp_att = np.resize(
                     attention_plot[:, features_to_keep[i][0]], (16, 16)
                 )
-                if threshold:
-                    temp_att /= np.max(temp_att)
-                    temp_att[temp_att < 0.1] = 0.0
+                minimum = np.min(temp_att)
+                maximum = np.max(temp_att)
+
+                if threshold and not global_threshold:
+                    if minimum < 0:
+                        temp_att /= np.abs(minimum)
+                    else:
+                        temp_att -= minimum
+                        temp_att /= maximum - minimum
+                    minimum = np.min(temp_att)
+                    maximum = np.max(temp_att)
+
+                if global_threshold:
+                    if global_minimum < 0:
+                        temp_att /= np.abs(global_minimum)
+                    else:
+                        temp_att -= global_minimum
+                        temp_att /= global_maximum - global_minimum
+                    minimum = -1 if global_minimum < 0 else 0
+                    maximum = 1
+
                 ax = fig.add_subplot(4, 4, j + 1)
                 title = "\n".join(textwrap.wrap(features_to_keep[i][1], 15))
-                ax.set_title(title, fontsize=25)
-                img = ax.imshow(temp_image, cmap="gray")
-                ax.imshow(temp_att, cmap="gray", alpha=0.6, extent=img.get_extent())
+                ax.set_title(title, fontsize=42, fontweight="bold")
+                img = ax.imshow(temp_image, cmap="gray", vmin=0, vmax=1)
+                cbar_data = ax.imshow(
+                    temp_att,
+                    cmap="coolwarm" if minimum < 0 else "gist_heat",
+                    alpha=0.6,
+                    vmin=minimum,
+                    vmax=maximum,
+                    extent=img.get_extent(),
+                )
+                cbar = fig.colorbar(cbar_data, ax=ax, extend="both")
+                cbar.minorticks_on()
                 ax.set_xticks([])
                 ax.set_yticks([])
                 j += 1
         else:
             temp_att = np.resize(attention_plot[:, i], (16, 16))
-            if threshold:
-                temp_att /= np.max(temp_att)
-                temp_att[temp_att < 0.1] = 0.0
+            minimum = np.min(temp_att)
+            maximum = np.max(temp_att)
+
+            if threshold and not global_threshold:
+                if minimum < 0:
+                    temp_att /= np.abs(minimum)
+                else:
+                    temp_att -= minimum
+                    temp_att /= maximum - minimum
+                minimum = np.min(temp_att)
+                maximum = np.max(temp_att)
+
+            if global_threshold:
+                if global_minimum < 0:
+                    temp_att /= np.abs(global_minimum)
+                else:
+                    temp_att -= global_minimum
+                    temp_att /= global_maximum - global_minimum
+                minimum = -1 if global_minimum < 0 else 0
+                maximum = 1
+
             ax = fig.add_subplot(6, 6, i + 1)
             title = "\n".join(
                 textwrap.wrap(
-                    config.datasets[config.dataset_identifier].feature_cols[i], 15
+                    config.datasets[config.dataset_identifier].feature_cols[i], 11
                 )
             )
-            ax.set_title(title, fontsize=25)
-            img = ax.imshow(temp_image, cmap="gray")
-            ax.imshow(temp_att, cmap="gray", alpha=0.6, extent=img.get_extent())
+            ax.set_title(title, fontsize=42, fontweight="bold")
+            img = ax.imshow(temp_image, cmap="gray", vmin=0, vmax=1)
+            cbar_data = ax.imshow(
+                temp_att,
+                cmap="coolwarm" if minimum < 0 else "gist_heat",
+                alpha=0.6,
+                vmin=minimum,
+                vmax=-minimum if minimum < 0 else maximum,
+                extent=img.get_extent(),
+            )
+            cbar = fig.colorbar(
+                cbar_data,
+                ax=ax,
+                extend="both",
+                ticks=np.linspace(
+                    minimum, -minimum if minimum < 0 else maximum, 9, endpoint=True
+                ),
+            )
+            cbar.minorticks_on()
             ax.set_xticks([])
             ax.set_yticks([])
 
@@ -97,8 +173,12 @@ def plot_attention(
     output_path = os.path.join(
         chkpt_dir,
         "attentions",
-        "corr" if only_correlating_cols else "",
-        "thresholded" if threshold else "",
+        "corr" if only_correlating_cols else "all",
+        "thresholded"
+        if threshold
+        else "non_thresholded"
+        if not global_threshold
+        else "global_threshold",
         f"attn_{IND}.png",
     )
     Path(output_path).parents[0].mkdir(exist_ok=True, parents=True)
@@ -112,6 +192,7 @@ def run_inference(
     plot_all: bool,
     threshold: bool,
     only_correlating_cols: bool,
+    global_threshold: bool,
 ):
     config_path = Path(chkpt_dir) / "config.yaml"
     config = omegaconf.OmegaConf.load(config_path)
@@ -155,6 +236,18 @@ def run_inference(
     b = D / (r + 1)
     a = r * b
 
+    min_a, max_a = np.min(a), np.max(a)
+    min_b, max_b = np.min(b), np.max(b)
+    # min-max scale
+    a -= min_a
+    a /= max_a - min_a
+
+    b -= min_b
+    b /= max_b - min_b
+
+    print(f"min_a : {np.min(a)} | max_a : {np.max(a)}")
+    print(f"min_b : {np.min(b)} | max_b : {np.max(b)}")
+
     discretized_test = test_predictions
     discretized_test[discretized_test >= 0.5] = 1
     discretized_test[discretized_test < 0.5] = 0
@@ -165,6 +258,17 @@ def run_inference(
     mean_mild = np.mean(attentions[mild_ind], axis=0)
     mean_severe = np.mean(attentions[severe_ind], axis=0)
 
+    output_path = os.path.join(
+        chkpt_dir,
+        "attentions",
+        "mean_severe_attentions.npy" if test else "valid_mean_severe_attentions.npy",
+    )
+    Path(output_path).parents[0].mkdir(exist_ok=True, parents=True)
+    np.save(output_path, mean_severe)
+    np.save(
+        output_path.replace("mean_severe_attentions", "mean_mild_attentions"), mean_mild
+    )
+
     plot_attention(
         a,
         mean_mild,
@@ -173,6 +277,7 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
     )
 
     plot_attention(
@@ -183,6 +288,7 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
     )
 
     plot_attention(
@@ -193,6 +299,7 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
     )
 
     plot_attention(
@@ -203,6 +310,7 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
     )
 
     plot_attention(
@@ -213,6 +321,7 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
     )
 
     plot_attention(
@@ -223,6 +332,7 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
     )
 
     plot_attention(
@@ -233,6 +343,29 @@ def run_inference(
         chkpt_dir,
         threshold,
         only_correlating_cols,
+        global_threshold,
+    )
+
+    plot_attention(
+        a,
+        mean_severe - mean_mild,
+        "a_mean_severe_minus_mild" if test else "valid_a_mean_severe_minus_mild",
+        config,
+        chkpt_dir,
+        threshold,
+        only_correlating_cols,
+        global_threshold,
+    )
+
+    plot_attention(
+        b,
+        mean_severe - mean_mild,
+        "b_mean_severe_minus_mild" if test else "valid_b_mean_severe_minus_mild",
+        config,
+        chkpt_dir,
+        threshold,
+        only_correlating_cols,
+        global_threshold,
     )
 
     if args.plot_all:
@@ -245,7 +378,14 @@ def run_inference(
                 test_images[IND],
             )
             plot_attention(
-                image, att, IND, config, chkpt_dir, threshold, only_correlating_cols
+                image,
+                att,
+                IND,
+                config,
+                chkpt_dir,
+                threshold,
+                only_correlating_cols,
+                global_threshold,
             )
 
     df = pd.DataFrame(columns=["file", "prognosis"])
@@ -288,6 +428,7 @@ if __name__ == "__main__":
     parser.add_argument("--plot_all", action="store_true", default=False)
     parser.add_argument("--threshold", action="store_true", default=False)
     parser.add_argument("--only_correlating_cols", action="store_true", default=False)
+    parser.add_argument("--global_threshold", action="store_true", default=False)
 
     args = parser.parse_args()
     run_inference(
@@ -296,4 +437,5 @@ if __name__ == "__main__":
         args.plot_all,
         args.threshold,
         args.only_correlating_cols,
+        args.global_threshold,
     )
